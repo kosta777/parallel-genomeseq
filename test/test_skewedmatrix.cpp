2c9ba2625dd1ed86bd9e7301b6517f965910652f
Langwen Huang <huanglangwen10@gmail.com>
2019-11-13 19:01:19 +0100

    improve test

--- test/test_skewedmatrix.cpp
+++ test/test_skewedmatrix.cpp
@@ -12,7 +12,8 @@ TEST(SimilarityMatrix,SkewedMatrixIndex) {
   auto size = len_x * len_y;
   Eigen::VectorXd initvec = Eigen::VectorXd::LinSpaced(size, 0., size - 1);
   auto raw_matrix = Eigen::MatrixXd(len_y, len_x);
-  auto true_matrix = Eigen::Map<Eigen::MatrixXd>(initvec.data(), len_x, len_y);
+  auto true_matrix = Eigen::MatrixXd(len_x, len_y);
+  //auto true_matrix = Eigen::Map<Eigen::MatrixXd>(initvec.data(), len_x, len_y);
   for (size_t j = 0; j < len_y; j++) {
     for (size_t i = 0; i < len_x; i++) {
       auto idx1 = index_tuple(i,j);
@@ -20,14 +21,17 @@ TEST(SimilarityMatrix,SkewedMatrixIndex) {
       ASSERT_EQ(idx1,skewed1.rawindex2trueindex(skewed1.trueindex2rawindex(idx1)));
       ASSERT_EQ(idx2,skewed2.rawindex2trueindex(skewed2.trueindex2rawindex(idx2)));
       ASSERT_EQ(idx2,skewed2.trueindex2rawindex(skewed2.rawindex2trueindex(idx2)));
-      auto [ri,rj] = skewed1.trueindex2rawindex(idx1);
-      raw_matrix(ri,rj) = true_matrix(i,j);
+      //raw_matrix(ri,rj) = true_matrix(i,j);
 #ifdef VERBOSE
-      std::cout << "True index: (" << i <<","<< j << "), Raw index: (" << ri <<","<< rj << ")" << std::endl;
+      auto [ri,rj] = skewed1.trueindex2rawindex(idx1);
+      true_matrix(i,j) = i + j;
+      raw_matrix(ri,rj) = i + j;
+      //std::cout << "True index: (" << i <<","<< j << "), Raw index: (" << ri <<","<< rj << ")" << std::endl;
 #endif
     }
   }
 #ifdef VERBOSE
-  std::cout << raw_matrix << std::endl;
+  std::cout << "Before Transform:\n" << true_matrix << std::endl;
+  std::cout << "After Transform:\n" <<raw_matrix << std::endl;
 #endif
 }

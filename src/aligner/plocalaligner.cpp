a670f653eaf6a0ace33f8dbb635da19b1d700514
Langwen Huang <huanglangwen10@gmail.com>
2019-11-14 20:56:19 +0100

    Fix string range bug

--- src/aligner/plocalaligner.cpp
+++ src/aligner/plocalaligner.cpp
@@ -2,57 +2,68 @@
 #include "smithwaterman.h"
 #include <string_view>
 #include <memory>
+#include <vector>
+#include <assert.h>
 
 template<class SMT, class LAT>
-OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence)
+OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence, int npiece, double overlap_ratio)
     :
-    OMPParallelLocalAligner(first_sequence, second_sequence, [](const char &a, const char &b) { return a == b ? 3.0 : -3.0; }, 2.0) {}
+    OMPParallelLocalAligner(first_sequence, second_sequence, npiece, overlap_ratio, [](const char &a, const char &b) { return a == b ? 3.0 : -3.0; }, 2.0) {}
 
 template<class SMT, class LAT>
-OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence, double gap_penalty) :
-    OMPParallelLocalAligner(first_sequence, second_sequence, [](const char &a, const char &b) { return a == b ? 3.0 : -3.0; }, gap_penalty) {}
+OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence, int npiece, double overlap_ratio, double gap_penalty) :
+    OMPParallelLocalAligner(first_sequence, second_sequence, npiece, overlap_ratio, [](const char &a, const char &b) { return a == b ? 3.0 : -3.0; }, gap_penalty) {}
 
 template<class SMT, class LAT>
-OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence,
+OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence, int npiece, double overlap_ratio,
                                                       std::function<double(const char &, const char &)> &&scoring_function) :
-    OMPParallelLocalAligner(first_sequence, second_sequence, std::move(scoring_function), 2.0) {}
+    OMPParallelLocalAligner(first_sequence, second_sequence, npiece, overlap_ratio, std::move(scoring_function), 2.0) {}
 
 template<class SMT, class LAT>
-OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence,
+OMPParallelLocalAligner<SMT, LAT>::OMPParallelLocalAligner(std::string_view first_sequence, std::string_view second_sequence, int npiece, double overlap_ratio,
                                                       std::function<double(const char &, const char &)> &&scoring_function,
                                                       double gap_penalty) :
     pos(0),
     max_score(-1),
     gap_penalty(gap_penalty),
-    overlap_ratio(2.0),
+    overlap_ratio(overlap_ratio),
+    npiece(npiece),
     sequence_x(first_sequence),
     sequence_y(second_sequence),
     scoring_function(std::move(scoring_function)) {}
 
-std::pair<size_t, size_t> _make_string_range(int ithread, int nthreads, size_t shortstringlength ,size_t longstringlength, double overlap_ratio) {
+std::vector<std::pair<size_t, size_t>> _make_string_range(int npiece, size_t shortstringlength ,size_t longstringlength, double overlap_ratio) {
   auto overlaplength = (size_t)(shortstringlength * overlap_ratio);
-  overlaplength = (overlaplength * 2) > (longstringlength / nthreads) ? longstringlength / (2 * nthreads) : overlaplength;
-  auto recurrencelength = (longstringlength - 2 * overlaplength) / nthreads;
-  return {ithread * recurrencelength, (ithread + 1) * recurrencelength + overlaplength * 2 };
+  std::vector<std::pair<size_t, size_t>> string_ranges;
+  auto piecelength = (longstringlength + (npiece - 1) * overlaplength) / npiece;
+  assert(overlaplength <= piecelength);
+  size_t left = 0;
+  size_t right = piecelength;
+  string_ranges.emplace_back(left, right);
+  while ((double)(longstringlength - right + overlaplength) > piecelength ) {
+    left = std::max((size_t)0, right - overlaplength);
+    right = std::min(left + piecelength, longstringlength);
+    if (longstringlength - right < std::min((size_t)20, piecelength - overlaplength)) right = longstringlength;
+    string_ranges.emplace_back(left, right);
+  }
+  if (right < longstringlength) string_ranges.emplace_back(std::max((size_t)0, right - overlaplength), longstringlength);
+  assert(string_ranges.size() == (size_t)npiece);
+  return string_ranges;
 }
 
 template<class SMT, class LAT>
 double OMPParallelLocalAligner<SMT, LAT>::calculateScore() {
-#ifdef USEOMP
-  auto nthreads = OMP_NUM_THREADS ;
-#else
-  auto nthreads = 1;
-#endif
   auto shortstringlength = sequence_x.size();
   auto longstringlength = sequence_y.size();
   double max_score_l = -1.0;
-  int max_score_thread = -1;
+  int max_score_piece = -1;
+  auto string_ranges = _make_string_range(npiece, shortstringlength, longstringlength, overlap_ratio);
 #ifdef USEOMP
-  #pragma omp parallel for default(none) shared(nthreads, shortstringlength, longstringlength, max_score_l, max_score_thread, \
+  #pragma omp parallel for default(none) shared(string_ranges, shortstringlength, longstringlength, max_score_l, max_score_piece, \
                                                 overlap_ratio, sequence_x, sequence_y, scoring_function, gap_penalty)
 #endif
-  for (int i = 0; i < nthreads; i++) {
-    auto [left, right] = _make_string_range(i, nthreads, shortstringlength, longstringlength, overlap_ratio);
+  for (int i = 0; i < npiece; i++) {
+    auto [left, right] = string_ranges[i];
     std::string_view sequence_y_i = sequence_y.substr(left, right - left);
     {
       auto smp = std::make_unique<SMT>(sequence_x, sequence_y_i);
@@ -60,12 +71,12 @@ double OMPParallelLocalAligner<SMT, LAT>::calculateScore() {
       auto [x, y, max] = smp -> find_index_of_maximum();
       if (max > max_score_l) {
         max_score_l = max;
-        max_score_thread = i;
+        max_score_piece = i;
       }
     }
   }
   max_score = max_score_l;
-  auto [left, right] = _make_string_range(max_score_thread, nthreads, shortstringlength, longstringlength, overlap_ratio);
+  auto [left, right] = string_ranges[max_score_piece];
   {
     std::string_view sequence_y_i = sequence_y.substr(left, right - left);
     auto la = std::make_unique<LAT>(sequence_x, sequence_y_i);
--- src/aligner/plocalaligner.h
+++ src/aligner/plocalaligner.h
@@ -5,10 +5,10 @@
 template <class Similarity_Matrix_Type, class LocalAligner_Type>
 class OMPParallelLocalAligner : public ParallelLocalAligner<Similarity_Matrix_Type, LocalAligner_Type> {
   public:
-    OMPParallelLocalAligner(std::string_view, std::string_view);
-    OMPParallelLocalAligner(std::string_view, std::string_view, double);
-    OMPParallelLocalAligner(std::string_view, std::string_view, std::function<double(const char &, const char &)> &&);
-    OMPParallelLocalAligner(std::string_view, std::string_view, std::function<double(const char &, const char &)> &&, double);
+    OMPParallelLocalAligner(std::string_view, std::string_view, int, double);
+    OMPParallelLocalAligner(std::string_view, std::string_view, int, double, double);
+    OMPParallelLocalAligner(std::string_view, std::string_view, int, double, std::function<double(const char &, const char &)> &&);
+    OMPParallelLocalAligner(std::string_view, std::string_view, int, double, std::function<double(const char &, const char &)> &&, double);
     double calculateScore() override;
     double getScore() const override { return max_score; };
     unsigned int getPos() const override { return pos; };
@@ -17,6 +17,7 @@ class OMPParallelLocalAligner : public ParallelLocalAligner<Similarity_Matrix_Ty
     double max_score;
     double gap_penalty;
     double overlap_ratio; // heuristics of maximum len(consensus_x)/len(sequence_x)
+    int npiece; // number of pieces that sequence_y is broken into
     std::string sequence_x;
     std::string sequence_y;
     std::function<double(const char &, const char &)> scoring_function;

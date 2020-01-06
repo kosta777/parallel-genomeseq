d693b326433ca2e0b0f2497a9468bca8341b6253
Han Yao Choong <hchoong@lo-login-01.leonhard.ethz.ch>
2020-01-06 16:00:53 +0100

    fix ompfg benchmarking shell scripts

--- benchmark/leonhardsetup.sh
+++ benchmark/leonhardsetup.sh
@@ -1,4 +1,3 @@
 module load gcc
 module load openmpi
-#module load "cmake/3.12.0"
-module --ignore-cache load cmake/3.12.0
+module load cmake
--- benchmark/ompfg/data_get_chr22.sh
+++ benchmark/ompfg/data_get_chr22.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-benchmark_dir=$(dirname `realpath $0`)
+ompfg_dir=$(dirname `realpath $0`)
+benchmark_dir=$(dirname $ompfg_dir)
 project_dir=$(dirname $benchmark_dir)
+echo $benchmark_dir
+echo $project_dir
 data_dir=$project_dir"/data"
+echo $data_dir
 
 ucsc_chr22_wget_cmd="wget \"http://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chr22.fa.gz\" -P "$data_dir
 eval $ucsc_chr22_wget_cmd
